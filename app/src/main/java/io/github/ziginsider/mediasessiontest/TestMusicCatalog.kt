2ee467df43114bbeadb3e7ed02f0a379a5d5bb54
ziginsider <ziginsider@gmail.com>
2018-06-24 18:10:42 +0300

    add TestMusicCatalog

--- app/src/main/java/io/github/ziginsider/mediasessiontest/TestMusicCatalog.kt
+++ app/src/main/java/io/github/ziginsider/mediasessiontest/TestMusicCatalog.kt
@@ -13,28 +13,55 @@ class TestMusicCatalog {
     val catalog = arrayOf(
             Track("Triangle",
                     "Jason Shaw",
-                    R.drawable.image266680,
+                    R.drawable.image1,
                     Uri.parse("https://freepd.com/Ballad/Triangle.mp3"),
                     (3 * 60 + 41) * 1000),
             Track("Rubix Cube",
                     "Jason Shaw",
-                    R.drawable.image396168,
+                    R.drawable.image2,
                     Uri.parse("https://freepd.com/Ballad/Rubix Cube.mp3"),
                     (3 * 60 + 44) * 1000),
             Track("MC Ballad S Early Eighties",
                     "Frank Nora",
-                    R.drawable.image533998,
+                    R.drawable.image3,
                     Uri.parse("https://freepd.com/Ballad/MC Ballad S Early Eighties.mp3"),
                     (2 * 60 + 50) * 1000),
             Track("Folk Song",
                     "Brian Boyko",
-                    R.drawable.image544064,
+                    R.drawable.image4,
                     Uri.parse("https://freepd.com/Acoustic/Folk Song.mp3"),
                     (3 * 60 + 5) * 1000),
             Track("Morning Snowflake",
                     "Kevin MacLeod",
-                    R.drawable.image208815,
+                    R.drawable.image5,
                     Uri.parse("https://freepd.com/Acoustic/Morning Snowflake.mp3"),
                     (2 * 60 + 0) * 1000)
     )
+
+    val maxTrackIndex = catalog.size - 1
+    var currentTrackIndex = 0
+
+    var currentTrack = catalog[0]
+        get() = catalog[currentTrackIndex]
+        private set
+
+    fun next(): Track {
+        if (currentTrackIndex == maxTrackIndex) {
+            currentTrackIndex = 0
+        } else {
+            currentTrackIndex++
+        }
+        return currentTrack
+    }
+
+    fun previous(): Track {
+        if (currentTrackIndex == 0) {
+            currentTrackIndex = maxTrackIndex
+        } else {
+            currentTrackIndex--
+        }
+        return currentTrack
+    }
+
+    
 }
